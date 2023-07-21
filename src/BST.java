10d2b58ea583e9f132377eb9474b861bda820632
Ufyanich <tima.sul.2001@gmail.com>
2023-07-21 16:11:07 +0600

    Creation of abstract Methods in Comments

--- src/BST.java
+++ src/BST.java
@@ -12,6 +12,9 @@ public class BST<K extends Comparable<K>, V> {
         }
     }
 
-    public void put(K key, V val)
+// void put(K key, V val) {} ;
+//    public V get(K key){};
+//    public void delete(K key){};
+//    public Iterable<K> iterator(){}
 
 }
